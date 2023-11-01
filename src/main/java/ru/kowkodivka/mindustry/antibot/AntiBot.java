593903f735f41484273aff2233dcee1824f8bc00
Kowkodivka <theeugenet@gmail.com>
2023-11-01 23:25:59 +0700

    Reformatting

--- src/main/java/ru/kowkodivka/mindustry/antibot/AntiBot.java
+++ src/main/java/ru/kowkodivka/mindustry/antibot/AntiBot.java
@@ -4,31 +4,63 @@ import arc.struct.Seq;
 import arc.util.Log;
 import mindustry.Vars;
 import mindustry.mod.Plugin;
-import mindustry.net.Administration;
 import mindustry.net.NetConnection;
 import mindustry.net.Packets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class AntiBot extends Plugin {
     public AntiBot() {
         Vars.net.handleServer(Packets.Connect.class, (con, packet) -> {
-            var connections = Seq.with(Vars.net.getConnections()).retainAll(other -> other.address.equals(con.address));
-            if (connections.size >= 2) {
-                try {
-                    if (!System.getProperty("os.name").toLowerCase().startsWith("windows")) {
-                        Runtime.getRuntime().exec(String.format("/bin/sh iptables -A INPUT %s -j DROP", con.address));
-                    }
-                } catch (IOException e) {
-                    Log.err("Something went wrong:\n@", e);
+            try {
+                String organization = getOrganization(con.address);
+                if (organization.contains("Microsoft Corporation")) {
+                    handleMicrosoftConnection(con);
                 }
-
-                // Уже есть на Darkdustry
-                if (!Administration.Config.serverName.name.toLowerCase().contains("darkdustry")) {
-                    Vars.netServer.admins.blacklistDos(con.address);
-                    connections.each(NetConnection::close);
-                }
+            } catch (IOException e) {
+                Log.err("Failed to check organization for IP: " + con.address, e);
             }
+
+            handleDuplicateConnections(con);
         });
+    }
+
+    private String getOrganization(String ipAddress) throws IOException {
+        URL url = new URL("https://ipinfo.io/" + ipAddress + "/org");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+    private void handleMicrosoftConnection(NetConnection connection) {
+        try {
+            if (!System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+                Runtime.getRuntime().exec(String.format("/bin/sh iptables -A INPUT %s -j DROP", connection.address));
+            }
+        } catch (IOException e) {
+            Log.err("Failed to block Microsoft connection for IP: " + connection.address, e);
+        }
+
+        Vars.netServer.admins.blacklistDos(connection.address);
+        connection.close();
+    }
+
+    private void handleDuplicateConnections(NetConnection connection) {
+        var connections = Seq.with(Vars.net.getConnections()).retainAll(other -> other.address.equals(connection.address));
+        if (connections.size >= 2) {
+            Vars.netServer.admins.blacklistDos(connection.address);
+            connections.each(NetConnection::close);
+        }
     }
 }
